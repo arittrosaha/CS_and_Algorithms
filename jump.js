639dece3f1cca6b5a24844423703dc90781da8de
Aritra Saha <arittrosaha@gmail.com>
2018-08-28 19:14:41 -0400

    jumpGraph and jumpLC

--- jump.js
+++ jump.js
@@ -1,16 +1,16 @@
-function jumpSlow(nums, idx = 0) { // O(n) : n!
+function jumpBF(nums, idx = 0) { // O(n) : n!
   if (idx === nums.length - 1) return 0;
   if (idx > nums.length - 1) return nums.length;
-  
+
   let max = nums[idx];
   let ways = [];
   for (let i = 1; i <= max; i++) {
-    ways.push(jumpSlow(nums, idx + i) + 1);
+    ways.push(jumpBF(nums, idx + i) + 1);
   }
  return Math.min(...ways);
 }
 
-function jumpFast(nums, idx = 0, memo = {}) {
+function jumpMemo(nums, idx = 0, memo = {}) {
   if (idx in memo) return memo[idx];
   if (idx === nums.length - 1) return 0;
   if (idx > nums.length - 1) return nums.length;
@@ -18,12 +18,59 @@ function jumpFast(nums, idx = 0, memo = {}) {
   let max = nums[idx];
   let ways = [];
   for (let i = 1; i <= max; i++) {
-    ways.push(jumpFast(nums, idx + i, memo) + 1);
+    ways.push(jumpMemo(nums, idx + i, memo) + 1);
   }
+
   memo[idx] = Math.min(...ways);
   return memo[idx];
 }
 
 let input = [2,3,1,1,4]; // => 2
-console.log(jumpSlow(input));
-console.log(jumpFast(input));
+console.log(jumpBF(input));
+console.log(jumpMemo(input));
+
+function jumpGraph(nums) {
+  if (nums.length === 1) return 0;
+
+  let stack = [ {index: 0, depth: 0} ];
+
+  while (stack.length) {
+    let node = stack.pop();
+
+    let neighbors = getNeighborIndices(nums, node.index);
+    for (let i = 0; i < neighbors.length; i++) {
+      let neighborIdx = neighbors[i];
+      let neighborDepth = node.depth + 1;
+      if (neighborIdx === nums.length - 1) return neighborDepth;
+      stack.push({ index: neighborIdx, depth: neighborDepth });
+    }
+  }
+}
+
+function getNeighborIndices(nums, idx) {
+  let neighbors = [];
+
+  for (let i = 1; i <= nums[idx]; i++) {
+    neighbors.push(idx + i);
+  }
+
+  return neighbors.sort((a, b) => (nums[a] + a - idx) - (nums[b] + b - idx));
+}
+
+var jumpLC = function(nums) {
+    let jumps = 0, currEnd = 0, currFarthest = 0;
+
+    for (let i = 0; i < nums.length-1; i++) {
+        currFarthest = Math.max(currFarthest, nums[i] + i);
+
+        if (currFarthest >= nums.length-1) {
+            jumps++;
+            break;
+        }
+        if (i === currEnd) {
+            jumps++;
+            currEnd = currFarthest;
+        }
+    }
+    return jumps;
+};
