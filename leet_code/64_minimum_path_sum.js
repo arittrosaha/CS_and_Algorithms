1c946d96f8ed7484507968e1f4daafd5593f8936
Aritra Saha <arittrosaha@gmail.com>
2018-09-10 19:36:25 -0400

    solve minimum_path_sum

--- leet_code/64_minimum_path_sum.js
+++ leet_code/64_minimum_path_sum.js
@@ -14,15 +14,73 @@
 // Output: 7
 // Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 
-var minPathSum = function(grid) {
-
+var minPathSum1 = function(grid) {
+    for(let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (i === 0 && j === 0) {
+                continue;
+            } else if (i === 0) {
+              grid[i][j] += grid[i][j-1];
+            } else if (j === 0) {
+              grid[i][j] += grid[i-1][j];
+            }
+            else {
+                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+    }
+    return grid[grid.length-1][grid[0].length-1];
 };
+
+console.log(minPathSum1([
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]));
 
 // memo = { row, col: shortest path sum from 0,0 to row, col }
 
-function minPathSum1(grid, row=0, col=0) {
+function treverse(grid, row=0, col=0) {
   if (row === grid.length || col === grid[0].length) return;
   console.log(grid[row][col]);
-  minPathSum1(grid, row, col + 1);
-  minPathSum1(grid, row + 1, col);
+  treverse(grid, row, col + 1);
+  treverse(grid, row + 1, col);
+}
+
+function minPathSum2(grid, row=0, col=0, memo = {}) {
+  let key = `${row}-${col}`;
+
+  // if my pos is invalid
+  if (row >= grid.length || col >= grid[0].length) return Infinity;
+  // if my pos is already at goal
+  if (row === grid.length - 1 && col === grid[0].length - 1) return grid[row][col];
+
+  if (key in memo) {
+    return memo[key];
+  }
+
+  let result = grid[row][col] + Math.min(
+    minPathSum2(grid, row + 1, col),
+    minPathSum2(grid, row, col + 1)
+  );
+
+  // if Infinity was not used, then use the following code:
+
+  // let down = minPathSum2(grid, row + 1, col, memo);
+  // let right = minPathSum2(grid, row, col + 1, memo);
+  //
+  // let result;
+  // if (right === null) {
+  //   result = grid[row][col] + down;
+  // } else if (down === null) {
+  //   result = grid[row][col] + right;
+  // } else {
+  //   result = grid[row][col] + Math.min(
+  //     minPathSum2(grid, row + 1, col),
+  //     minPathSum2(grid, row, col + 1)
+  //   );
+  // }
+
+  memo[key] =result;
+  return memo[key];
 }
