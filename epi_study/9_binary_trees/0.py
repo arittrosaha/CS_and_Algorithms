31cefdfba6b3af826e67b09742e918e6ec5d3ad3
Aritra Saha <arittrosaha@gmail.com>
2019-06-05 18:48:45 -0400

    Test if a binary tree is height-balanced 9.1

---
+++ epi_study/9_binary_trees/0.py
@@ -0,0 +1,65 @@
+# Binary - at most 2 children
+# full - 0 or all children at every node
+# perfect - all leafs at same level
+# complete - all levels expect the bottom are filled with max number of childrens
+#            AND bottom level should be filled left to right
+# Tree - no cycles
+# Balanced - difference between height of left and right subtree is <= 1 AND left subtree is balanced
+#                                                                        AND right subtree is balanced
+
+# Relationship between n, number of nodes and h, height of binary tree:
+# height from nodes:
+# -> maximum possible height => h : n - 1
+# 4        - h : 0
+#  \
+#   3      - h : 1
+#    \
+#     2    - h : 2
+#      \
+#       1  - h : 3
+# -> minimum possible height => h : log2n
+# at every depth, we have twice the amount of n
+
+# -> nodes from height:
+# -> minimum n : h + 1
+# for the first edge, there are two nodes at each end. Then after for every edge, there is a node at the end
+# -> maximum n : (2^(h+1)) - 1
+# ref -> https://www.quora.com/What-is-the-maximum-number-of-nodes-in-a-binary-tree-Is-it-2-h-1-or-2-h+1-1/answer/Amit-Jaiswal-452
+
+
+# ref -> https://www.geeksforgeeks.org/relationship-number-nodes-height-binary-tree/
+
+
+# Full binary tree - the number of nonleaf nodes in a full binary tree is one less than the number of leaves.
+# Perfect binary tree - contains exactly (2^(h+1)) - 1 nodes, of which 2^h are leaves
+# Complete binary tree - has a height of log2n for n nodes
+
+
+# Two types of skewed binary tree:
+# left-skewed binary tree - no right child
+# right-skewed binary tree - no left child
+
+
+# Traversel
+# Inorder - left -> root -> right
+# Preorder - root -> left -> right
+# Postorder - left -> right -> root
+
+# Recursive implementation : O(n) time and O(h) space for the depth of call stack
+# If each node has a parent attribute : O(n) and O(1) space iteratively
+
+def tree_traversal(root):
+    # comment back in the specific traversel line that is in need
+    if root:
+        # print("Preorder: %d" % root.data)
+        tree_traversal(root.left)
+
+        # print("Inorder: %d" % root.data)
+        tree_traversal(root.right)
+
+        # print("Postorder: %d" % root.data)
+
+
+class BinaryTree:
+	def __init__(self):
+    	self.left = self.right = self.parent = None
---
+++ epi_study/9_binary_trees/1.py
@@ -0,0 +1,92 @@
+# Test if a binary tree is height-balanced
+
+# Background - height-balanced aka balanced binary tree. It does not have to be perfect or complete
+# Prompt - A program that takes the root of a binary tree and checks whether the tree is height-balanced
+
+def is_balanced_binary_tree(tree): # Time: O(n) ; Space: O(h)
+    def height_diff(tree):
+        if not tree:
+            return -1 # -1 rather than 0 because if height is 0 at leaf, a stack beyond should be -1
+        
+        left_height = height_diff(tree.left)
+        if left_height == False and left_height != 0:
+            return False
+        right_height = height_diff(tree.right)
+        if right_height == False and right_height != 0:
+            return False
+
+        left_height += 1
+        right_height += 1
+        if abs(left_height - right_height) > 1:
+            return False
+        else:
+            return max(left_height, right_height)
+
+
+    if height_diff(tree) is not False:
+        return True
+    else:
+        return False
+
+
+class BinaryTree:
+    def __init__(self, data=None):
+        self.data = data
+        self.left = self.right = self.parent = None
+
+
+A = BinaryTree("A")
+B = BinaryTree("B")
+C = BinaryTree("C")
+D = BinaryTree("D")
+E = BinaryTree("E")
+F = BinaryTree("F")
+G = BinaryTree("G")
+H = BinaryTree("H")
+I = BinaryTree("I")
+J = BinaryTree("J")
+K = BinaryTree("K")
+L = BinaryTree("L")
+M = BinaryTree("M")
+N = BinaryTree("N")
+O = BinaryTree("O")
+
+A.left = B
+A.right = K
+B.left = C
+B.right = H
+C.left = D
+C.right = G
+D.left = E
+D.right = F
+H.left = I
+H.right = J
+K.left = L
+K.right = O # if exist, then it a balanced tree other wise false
+L.left = M
+L.right = N
+
+# print(is_balanced_binary_tree(A))
+
+import collections
+def is_balanced_binary_tree_EPI(tree): # logically, this solution is fundamentally like mine but done lot more properly 
+    # making tuples with attributes
+    BalancedStatusWithHeight = collections.namedtuple("BalancedStatusWithHeight", ("balanced", "height"))
+
+    def check_balanced(tree):
+        if not tree:
+            return BalancedStatusWithHeight(True, -1)
+        
+        left_result = check_balanced(tree.left)
+        if not left_result.balanced:
+            return BalancedStatusWithHeight(False, 0)
+        right_result = check_balanced(tree.right)
+        if not right_result.balanced:
+            return BalancedStatusWithHeight(False, 0)
+
+        is_balanced = abs(left_result.height - right_result.height) <= 1
+        height = max(left_result.height, right_result.height) + 1
+        return BalancedStatusWithHeight(is_balanced, height)
+    
+    return check_balanced(tree).balanced
+
